ea1ab2b1c7451ae0a87a5cc6d201d5d1238c5229
Clinton N. Dreisbach <crnixon@gmail.com>
2013-12-22 12:56:46 -0500

    initial commit

---
+++ project.clj
@@ -0,0 +1,8 @@
+(def boot-config (read-string (slurp "boot.edn")))
+
+(defproject codemash-todo "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies ~(:dependencies boot-config))
---
+++ src/codemash_todo/main.clj
@@ -0,0 +1,26 @@
+(ns codemash-todo.main
+  (:use [org.httpkit.server :only [run-server]])
+  (:require [ring.middleware.reload :as reload]
+            [tailrecursion.boot.core :refer [deftask]]))
+
+(def in-dev? (atom false))
+
+(defn app
+  [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "hello HTTP!"})
+
+(deftask server
+  [boot]
+  (fn [continue]
+    (fn [event]
+      (let [app (if @in-dev?
+                  (reload/wrap-reload app)
+                  app)]
+        (.waitFor (run-server app {:port 3000}))))))
+
+(deftask dev-server
+  [boot]
+  (reset! in-dev? true)
+  (server boot))
